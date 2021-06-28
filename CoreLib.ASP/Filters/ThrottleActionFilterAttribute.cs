b425691f208a3c3a0c09c9c86e01ca2a243cdb0f
Сергей <exluzzzivo@gmail.com>
2021-06-28 10:29:08 +0300

    Concurrent semaphore and throttling action attribute based on it

---
+++ CoreLib.ASP/Filters/ThrottleActionFilterAttribute.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using CoreLib.CORE.Helpers.StringHelpers;
+using CoreLib.CORE.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreLib.ASP.Filters
+{
+    /// <summary>
+    /// An action filter attribute that limits the execution of the target action in different ways
+    /// </summary>
+    public class ThrottleActionFilterAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// An unique key that is used to store the connection info in <see cref="IMemoryCache"/>
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The time in milliseconds clients must wait before executing the target action
+        /// </summary>
+        public long ThrottlingPeriod { get; set; }
+
+        /// <summary>
+        /// The number of allowed connections for clients during the <see cref="ThrottlingPeriod"/> an limited by the <see cref="ThrottlingBy"/>
+        /// </summary>
+        /// <remarks>
+        /// Default value: 1
+        /// </remarks>
+        public int MaximumConnectionsCountBeforeThrottling { get; set; } = 1;
+
+        /// <summary>
+        /// The type of the limitation
+        /// </summary>
+        /// <remarks>
+        /// Default value: <see cref="ThrottlingType.ByIp"/>
+        /// </remarks>
+        public ThrottlingType ThrottlingBy { get; set; } = ThrottlingType.ByIp;
+
+        public override async void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (Key.IsNullOrEmptyOrWhiteSpace())
+            {
+                throw new ArgumentNullException(nameof(Key));
+            }
+
+            if (MaximumConnectionsCountBeforeThrottling < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(MaximumConnectionsCountBeforeThrottling));
+            }
+
+            if (ThrottlingPeriod <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ThrottlingPeriod));
+            }
+
+            var throttlingTypeKeyPart = ThrottlingBy.ToString();
+
+            switch (ThrottlingBy)
+            {
+                case ThrottlingType.ByIp:
+                    throttlingTypeKeyPart += $":{context.HttpContext.Connection.RemoteIpAddress}";
+
+                    break;
+                case ThrottlingType.ByUser:
+                    if (context.HttpContext.User.Identity.IsAuthenticated)
+                    {
+                        throttlingTypeKeyPart += $":{context.HttpContext.User.Identity.Name}";
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                    break;
+            }
+
+            var key = $"{nameof(ThrottleActionFilterAttribute)}:{Key}:{throttlingTypeKeyPart}";
+
+            var memoryCache = context.HttpContext.RequestServices.GetService<IMemoryCache>();
+
+            var semaphore = await ConcurrentSemaphore.WaitAsync(key);
+
+            try
+            {
+                if (!memoryCache.TryGetValue(key, out Tuple<int, DateTimeOffset> throttleData))
+                {
+                    var expirationTime = DateTimeOffset.Now.Add(TimeSpan.FromMilliseconds(ThrottlingPeriod));
+
+                    memoryCache.Set(key,
+                        new Tuple<int, DateTimeOffset>(MaximumConnectionsCountBeforeThrottling, expirationTime),
+                        expirationTime);
+                }
+                else
+                {
+                    if (throttleData.Item1 <= 1)
+                    {
+                        context.Result = new ConflictResult();
+                    }
+                    else
+                    {
+                        memoryCache.Set(key, new Tuple<int, DateTimeOffset>(throttleData.Item1 - 1, throttleData.Item2),
+                            throttleData.Item2);
+                    }
+                }
+            }
+            finally
+            {
+                semaphore.Dispose();
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        /// <summary>
+        /// Type of the limitation
+        /// </summary>
+        public enum ThrottlingType : byte
+        {
+            /// <summary>
+            /// Limits requests by clients Ip address
+            /// </summary>
+            ByIp,
+
+            /// <summary>
+            /// Limits requests by identity user name
+            /// </summary>
+            ByUser,
+
+            /// <summary>
+            /// Limits all requests
+            /// </summary>
+            All
+        }
+    }
+}
---
+++ CoreLib.CORE/Types/ConcurrentSemaphore.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreLib.CORE.Types
+{
+    // https://stackoverflow.com/questions/31138179/asynchronous-locking-based-on-a-key/
+    /// <summary>
+    /// A helper class that provides methods for properly handling <see cref="SemaphoreSlim"/>s for concurrent operations
+    /// </summary>
+    public static class ConcurrentSemaphore
+    {
+        /// <summary>
+        /// A helper class to handle the current number of references to the provided <see cref="SemaphoreSlim"/>
+        /// </summary>
+        private class LockedSemaphore : IDisposable
+        {
+            internal LockedSemaphore(object key, SemaphoreSlim semaphore)
+            {
+                RefCount = 1;
+                Key = key;
+                Semaphore = semaphore;
+            }
+
+            /// <summary>
+            /// The current number of references to this semaphore
+            /// </summary>
+            internal int RefCount { get; set; }
+
+            /// <summary>
+            /// Semaphore
+            /// </summary>
+            internal SemaphoreSlim Semaphore { get; }
+
+            /// <summary>
+            /// Semaphore's unique key
+            /// </summary>
+            internal object Key { get; }
+
+            public void Dispose()
+            {
+                lock (Semaphores)
+                {
+                    --RefCount;
+
+                    if (RefCount == 0)
+                    {
+                        Semaphores.Remove(Key);
+                    }
+                }
+
+                Semaphore.Release();
+            }
+        }
+
+        /// <summary>
+        /// The global collection of semaphores currently in use
+        /// </summary>
+        private static readonly Dictionary<object, LockedSemaphore> Semaphores
+            = new Dictionary<object, LockedSemaphore>();
+
+        /// <summary>
+        /// Gets or creates a new <see cref="SemaphoreSlim"/> with the specified <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">Unique key</param>
+        /// <returns>A special semaphore object with the specified <paramref name="key"/> and the current number of references to it, including the current reference</returns>
+        private static LockedSemaphore GetOrCreate(object key)
+        {
+            LockedSemaphore item;
+
+            lock (Semaphores)
+            {
+                if (Semaphores.TryGetValue(key, out item))
+                {
+                    ++item.RefCount;
+                }
+                else
+                {
+                    item = new LockedSemaphore(key, new SemaphoreSlim(1, 1));
+                    Semaphores[key] = item;
+                }
+            }
+
+            return item;
+        }
+
+        /// <summary>
+        /// Waits to enter the <see cref="SemaphoreSlim"/> with the specified <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">Unique key</param>
+        /// <returns>An <see cref="IDisposable"/> object that should be disposed after the concurrent operation completes</returns>
+        public static IDisposable Wait(object key)
+        {
+            var semaphore = GetOrCreate(key);
+            semaphore.Semaphore.Wait();
+
+            return semaphore;
+        }
+
+        /// <summary>
+        /// Asynchronously waits to enter the <see cref="SemaphoreSlim"/> with the specified <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">Unique key</param>
+        /// <returns>A task that results in an <see cref="IDisposable"/> object that should be disposed after the concurrent operation completes</returns>
+        public static async Task<IDisposable> WaitAsync(object key)
+        {
+            var semaphore = GetOrCreate(key);
+            await semaphore.Semaphore.WaitAsync().ConfigureAwait(false);
+
+            return semaphore;
+        }
+    }
+}
