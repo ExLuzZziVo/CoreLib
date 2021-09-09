84e6b2d91294f881f02ca1799dd4fe51d41e415a
Сергей <exluzzzivo@gmail.com>
2021-09-09 17:07:58 +0300

    Safe file compression

--- CoreLib.CORE.Extensions.Compression/Helpers/FileHelpers/FileManipulator_Compression.cs
+++ CoreLib.CORE.Extensions.Compression/Helpers/FileHelpers/FileManipulator_Compression.cs
@@ -29,7 +29,7 @@ namespace CoreLib.CORE.Extensions.Compression.Helpers.FileHelpers
             using (var archive = ZipFile.Open(zipPath,
                 File.Exists(zipPath) ? ZipArchiveMode.Update : ZipArchiveMode.Create))
             {
-                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+                archive.CreateEntryFromFileSafely(filePath);
             }
         }
 
@@ -55,7 +55,67 @@ namespace CoreLib.CORE.Extensions.Compression.Helpers.FileHelpers
             {
                 foreach (var fPath in filePaths)
                 {
-                    archive.CreateEntryFromFile(fPath, Path.GetFileName(fPath));
+                    archive.CreateEntryFromFileSafely(fPath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Packs specified directory to zip archive
+        /// </summary>
+        /// <param name="directoryPath">Paths to directory</param>
+        /// <param name="zipPath">Path to new zip archive. If null, it creates zip archive in the directory where the specified directory is located</param>
+        public static void PackDirectoryToZip(string directoryPath, string zipPath = null)
+        {
+            if (directoryPath.IsNullOrEmptyOrWhiteSpace())
+            {
+                throw new ArgumentNullException(nameof(directoryPath));
+            }
+
+            if (zipPath.IsNullOrEmptyOrWhiteSpace())
+            {
+                zipPath = $"{directoryPath.TrimEnd('\\', '/')}.zip";
+            }
+
+            using (var archive = ZipFile.Open(zipPath,
+                File.Exists(zipPath) ? ZipArchiveMode.Update : ZipArchiveMode.Create))
+            {
+                foreach (var fPath in Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories))
+                {
+                    archive.CreateEntryFromFileSafely(fPath, directoryPath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a zip archive entry even if the file is busy
+        /// </summary>
+        /// <param name="archive">Zip archive</param>
+        /// <param name="filePath">Path to the target file</param>
+        /// <param name="fileRootDirectoryPath">Path to the root directory of the target file. Is used only to pack a directory with subdirectories</param>
+        private static void CreateEntryFromFileSafely(this ZipArchive archive, string filePath, string fileRootDirectoryPath = null)
+        {
+            var file = new FileInfo(filePath);
+            var entryFullName = file.Name;
+            
+            if (!fileRootDirectoryPath.IsNullOrEmptyOrWhiteSpace())
+            {
+                entryFullName = filePath.Replace(fileRootDirectoryPath, string.Empty).TrimStart('\\','/');
+            }
+            
+            if (archive.Mode == ZipArchiveMode.Update && archive.Entries.Any(e => e.FullName == entryFullName && e.Length == file.Length))
+            {
+                return;
+            }
+
+            var entry = archive.CreateEntry(entryFullName);
+            entry.LastWriteTime = file.LastWriteTime;
+
+            using (var fs = file.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var es = entry.Open())
+                {
+                    fs.CopyTo(es);
                 }
             }
         }
